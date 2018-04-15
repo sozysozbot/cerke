f1436ad9dfb7439ddf340c101b931ca85cf601c8
sozysozbot <sozysozbot@gmail.com>
2018-04-16 05:41:52 +0900

    漢字も変数名にしておく

--- cfs/app/Piece2.hs
+++ cfs/app/Piece2.hs
@@ -10,12 +10,25 @@ module Piece2
 ,PhantomPiece
 ,match
 ,getSide
+,船, 兵, 弓, 車, 虎, 馬, 筆, 巫, 将, 王
 ) where
 data Color = Kok1 | Huok2 deriving(Show, Eq, Ord)
 data Side = Upward | Downward deriving(Show, Eq, Ord)
 data Piece = Tam2 | Piece {color :: Color, prof :: Profession, side :: Side} deriving(Show, Eq, Ord)
 data Profession = Nuak1 | Kauk2 | Gua2 | Kaun1 | Dau2 | Maun1 | Kua2 | Tuk2 | Uai1 | Io deriving(Show, Eq, Ord, Enum)
 type ProfOrTam = Either () Profession -- Left () is tam2
+
+船, 兵, 弓, 車, 虎, 馬, 筆, 巫, 将, 王 :: Profession
+船 = Nuak1
+兵 = Kauk2
+弓 = Gua2
+車 = Kaun1
+虎 = Dau2
+馬 = Maun1
+筆 = Kua2
+巫 = Tuk2
+将 = Uai1
+王 = Io
 
 profToLin :: Profession -> Char
 profToLin p = "船兵弓車虎馬筆巫将王" !! fromEnum p
--- cfs/app/Piece3.hs
+++ cfs/app/Piece3.hs
@@ -9,5 +9,6 @@ module Piece3
 ,PhantomPiece
 ,match
 ,getSide
+,船, 兵, 弓, 車, 虎, 馬, 筆, 巫, 将, 王
 ) where
 import Piece2
