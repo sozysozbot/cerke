11df33df60d62080b74865a9720a59df5497816f
sozysozbot <sozysozbot@gmail.com>
2018-04-18 07:43:33 +0900

    relocate definitions; also, explicitly define initialBoard
    the fact that loadBoard can generate initialBoard should be tested by Spec, not by the definition itself of initialBoard

--- cerkefs/src/CerkeFS/Board2.hs
+++ cerkefs/src/CerkeFS/Board2.hs
@@ -2,5 +2,15 @@ module CerkeFS.Board2
 (M
 ,Board1
 ,Error(..)
+,sqKA,  sqLA,  sqNA,  sqTA,  sqZA,  sqXA,  sqCA,  sqMA,  sqPA, 
+ sqKE,  sqLE,  sqNE,  sqTE,  sqZE,  sqXE,  sqCE,  sqME,  sqPE, 
+ sqKI,  sqLI,  sqNI,  sqTI,  sqZI,  sqXI,  sqCI,  sqMI,  sqPI, 
+ sqKU,  sqLU,  sqNU,  sqTU,  sqZU,  sqXU,  sqCU,  sqMU,  sqPU, 
+ sqKO,  sqLO,  sqNO,  sqTO,  sqZO,  sqXO,  sqCO,  sqMO,  sqPO, 
+ sqKY,  sqLY,  sqNY,  sqTY,  sqZY,  sqXY,  sqCY,  sqMY,  sqPY, 
+ sqKAI, sqLAI, sqNAI, sqTAI, sqZAI, sqXAI, sqCAI, sqMAI, sqPAI,
+ sqKAU, sqLAU, sqNAU, sqTAU, sqZAU, sqXAU, sqCAU, sqMAU, sqPAU,
+ sqKIA, sqLIA, sqNIA, sqTIA, sqZIA, sqXIA, sqCIA, sqMIA, sqPIA
+,sqList
 ) where
 import CerkeFS.Internal.Board
