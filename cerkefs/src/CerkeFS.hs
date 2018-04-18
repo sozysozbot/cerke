f6051051eac6e087149596a0eb2ecfcf66377e75
sozysozbot <sozysozbot@gmail.com>
2018-04-18 09:37:42 +0900

    Move playFromStart to CerkeFS.hs

--- cerkefs/src/CerkeFS.hs
+++ cerkefs/src/CerkeFS.hs
@@ -4,9 +4,14 @@ module CerkeFS
 ,module CerkeFS.PrettyPrint
 ,module Control.Monad.Trans.State.Lazy
 ,module CerkeFS.Piece3
+,playFromStart
 ) where
 import CerkeFS.Board2
 import CerkeFS.GameState
 import CerkeFS.PrettyPrint
 import Control.Monad.Trans.State.Lazy
 import CerkeFS.Piece3
+import CerkeFS.InitialBoard
+
+playFromStart :: Monad m => StateT Fullboard m a -> m Fullboard
+playFromStart p = execStateT p Fullboard{board = initialBoard, hand = []}
