128879d5b692db35dcc8067372543269f2273059
sozysozbot <sozysozbot@gmail.com>
2018-04-27 20:05:01 +0900

    REPL

--- cerkefs/app/Main.hs
+++ cerkefs/app/Main.hs
@@ -3,20 +3,52 @@ module Main
 )
 where
 import CerkeFS
+import System.IO
 
 main :: IO ()
 main = do
-  putStrLn "Hello, World!"
+  guide
+  bar
 
+bar :: IO ()
+bar = do
+ --hSetBuffering stdout NoBuffering
+ putStr "CerkeFS> "
+ hFlush stdout
+ arr <- words <$> getLine
+ case arr of
+  ("quit":_) -> return ()
+  ("exit":_) -> return ()
+  [] -> bar
+  (x:xs) -> do
+   command x xs
+   bar
 
-loadFile :: FilePath -> IO Board1
-loadFile file = do
+command :: String -> [String] -> IO ()
+command "load_ascii" [] = putStrLn explain_load_ascii
+command "load_ascii" (path:_) = do
+ loadAsciiBoard' path
+command x xs = do
+  putStrLn $ "Unknown command " ++ x ++ "."
+
+guide :: IO ()
+guide = putStr $ unlines 
+ ["Guide:"
+ ,"    quit: exits the application."
+ ,"    exit: exits the application."
+ ,explain_load_ascii
+ ]
+
+explain_load_ascii = "    load_ascii FILENAME: loads the ASCII representation of the board."
+
+loadAsciiBoard :: FilePath -> IO Board1
+loadAsciiBoard file = do
  str <- readFile file
  let Just b = loadBoard str
  return b
 
-loadFile' :: String -> IO ()
-loadFile' file = do
+loadAsciiBoard' :: String -> IO ()
+loadAsciiBoard' file = do
  putStrLn "--------------------"
  putStrLn $ "Loading " ++ file
  str <- readFile file
