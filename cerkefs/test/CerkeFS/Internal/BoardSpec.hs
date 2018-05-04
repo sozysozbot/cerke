bdc9bd68d454a71bfb0d85ebf853c0a2741e8b43
sozysozbot <sozysozbot@gmail.com>
2018-05-04 15:03:30 +0900

    refactor sq `elem` inherentTam2Hue', and it is now significantly faster (tested 10 times)

--- cerkefs/test/CerkeFS/Internal/BoardSpec.hs
+++ cerkefs/test/CerkeFS/Internal/BoardSpec.hs
@@ -10,9 +10,16 @@ import Data.Maybe
 spec :: Spec
 spec = do
   describe "getNeighborsAndSelf" $ mapM_ u [0..80]
+  describe "isInherentTam2Hue'" $ mapM_ v [0..80]
+
 
 u i = do
     it ("getNeighborsAndSelf "++show i) $ 
       getNeighborsAndSelf i `shouldBeSimilarTo` getNeighborsAndSelf_old i 
+
+v i = do
+    it ("isInherentTam2Hue' "++show i) $
+     isInherentTam2Hue' i `shouldBe` (i `elem` map fromSquare [sqNI, sqNAI, sqTU, sqTY, sqZO, sqXU, sqXY, sqCI, sqCAI])
+
 
 getNeighborsAndSelf_old sq = mapMaybe (`add_` sq) [Vec a b | a <- [-1,0,1], b <- [-1,0,1]]
