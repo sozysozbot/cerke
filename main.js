6deb3b867422640739a864987377e507a9ad6df4
sozysozbot <sozysozbot@gmail.com>
2019-02-02 18:47:06 +0900

    prettier

--- main.js
+++ main.js
@@ -1,105 +1,105 @@
 var img_board = new Image();
- 	img_board.src = "cert_kaik.png"; 
+img_board.src = "cert_kaik.png";
 
 var ctx = null;
 
 //GUI
-function getHeight()
-{
-	return +document.getElementById('height').value;
+function getHeight() {
+  return +document.getElementById("height").value;
 }
 
-function getPaddings()
-{
-	var height = getHeight();
-	// top, right, bottom, left
-	return [50,10,50,10].map(function(x){return x*height/400;});
+function getPaddings() {
+  var height = getHeight();
+  // top, right, bottom, left
+  return [50, 10, 50, 10].map(function(x) {
+    return (x * height) / 400;
+  });
 }
 
-function init()
-{
-	ctx = document.getElementById("cv").getContext("2d");
+function init() {
+  ctx = document.getElementById("cv").getContext("2d");
 }
 
-function draw1(){
-	var height = getHeight();
-	var pads = getPaddings();
-	var full_w = (height + pads[1] + pads[3])
-	var full_h = (height + pads[0] + pads[2])
+function draw1() {
+  var height = getHeight();
+  var pads = getPaddings();
+  var full_w = height + pads[1] + pads[3];
+  var full_h = height + pads[0] + pads[2];
 
-	document.getElementById("cv").width = "" + full_w;
-	document.getElementById("cv").height = "" + full_h;
+  document.getElementById("cv").width = "" + full_w;
+  document.getElementById("cv").height = "" + full_h;
 
-	if(!document.getElementById('transparent').checked) {
-		ctx.fillStyle = "white";
-		ctx.fillRect(0, 0, full_w, full_h);
-	}
+  if (!document.getElementById("transparent").checked) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, full_w, full_h);
+  }
 
-	ctx.drawImage(img_board,pads[3],pads[0],height,height);
+  ctx.drawImage(img_board, pads[3], pads[0], height, height);
 }
 
-function drawChar(x, y, txt, isRed, isReversed)
-{
-	var height = getHeight();
+function drawChar(x, y, txt, isRed, isReversed) {
+  var height = getHeight();
 
-	ctx.font = Math.round(height * 0.085)+"px PekTak"; 
-	ctx.fillStyle = isRed ? "red" : "black";
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle";
+  ctx.font = Math.round(height * 0.085) + "px PekTak";
+  ctx.fillStyle = isRed ? "red" : "black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
 
-	var pads = getPaddings();
+  var pads = getPaddings();
 
-	var X = pads[3] + (x + 1/2) * height / 9;
-	var Y = pads[0] + (y + 1/2) * height / 9;
+  var X = pads[3] + ((x + 1 / 2) * height) / 9;
+  var Y = pads[0] + ((y + 1 / 2) * height) / 9;
 
-	if(isReversed){
-		ctx.rotate(Math.PI);
-		ctx.fillText(txt, -X, -Y);
-		ctx.rotate(Math.PI);
-	} else {
-		ctx.fillText(txt, X, Y);
-	}
+  if (isReversed) {
+    ctx.rotate(Math.PI);
+    ctx.fillText(txt, -X, -Y);
+    ctx.rotate(Math.PI);
+  } else {
+    ctx.fillText(txt, X, Y);
+  }
 }
 
-function error(txt){alert(txt)}
+function error(txt) {
+  alert(txt);
+}
 
 var conv_obj = {
-	"!": "船", 
-	"0": "無", 
-	"1": "兵", 
-	"2": "弓", 
-	"3": "車", 
-	"4": "虎", 
-	"5": "馬", 
-	"6": "筆", 
-	"7": "巫", 
-	"8": "将", 
-	"#": "王", 
-	"$": "皇"
+  "!": "船",
+  "0": "無",
+  "1": "兵",
+  "2": "弓",
+  "3": "車",
+  "4": "虎",
+  "5": "馬",
+  "6": "筆",
+  "7": "巫",
+  "8": "将",
+  "#": "王",
+  $: "皇"
+};
+
+function load_board() {
+  draw1();
+  var info = document.getElementById("load_board").value;
+  var res_arr = parser(info);
+  if (res_arr.length !== 81) {
+    error(
+      "Parse error: Expected 81 elements, but got " +
+        res_arr.length +
+        " element(s)"
+    );
+    return null;
+  }
+
+  for (var i = 0; i < 81; i++) {
+    if (res_arr[i]) {
+      drawChar(
+        i % 9,
+        (i / 9) | 0,
+        res_arr[i].txt,
+        res_arr[i].isRed,
+        res_arr[i].isReversed
+      );
+    }
+  }
 }
-
-
-function load_board()
-{
-	draw1();
-	var info = document.getElementById("load_board").value;
-	var res_arr = parser(info);
-	if(res_arr.length !== 81) {
-		error("Parse error: Expected 81 elements, but got " + res_arr.length + " element(s)");
-		return null;
-	}
-
-	for(var i=0; i<81; i++){
-		if(res_arr[i]) {
-			drawChar(i%9, (i/9)|0, res_arr[i].txt, res_arr[i].isRed, res_arr[i].isReversed);
-		}
-	}
-
-}
-
-
-
-
-
-
-
--- parse.js
+++ parse.js
@@ -1,61 +1,67 @@
-function parser(info)
-{
-	var res_arr = [];
+function parser(info) {
+  var res_arr = [];
 
-	// 0 "^" 1 "#" 2 "h" 0
-	var state = 0;
+  // 0 "^" 1 "#" 2 "h" 0
+  var state = 0;
 
-	var tmp = {};
+  var tmp = {};
 
-	for(var cnt = 0; cnt < info.length; cnt++){
-		if(/\s/.test(info[cnt])) {
-			continue;
-		}
+  for (var cnt = 0; cnt < info.length; cnt++) {
+    if (/\s/.test(info[cnt])) {
+      continue;
+    }
 
-		if(state === 0){
-			if(info[cnt] === "^" || info[cnt] === "_") {
-				tmp.isReversed = (info[cnt] === "_");
-				state++;
-				continue;
-			}
+    if (state === 0) {
+      if (info[cnt] === "^" || info[cnt] === "_") {
+        tmp.isReversed = info[cnt] === "_";
+        state++;
+        continue;
+      }
 
-			if(info[cnt] === "-") {
-				res_arr[res_arr.length] = null; // empty slot
-				tmp = {};
-				
-				state = 0; // explicit state0
+      if (info[cnt] === "-") {
+        res_arr[res_arr.length] = null; // empty slot
+        tmp = {};
 
-				continue;
-			}
+        state = 0; // explicit state0
 
-			error("Parse error: unexpected `" + info[cnt] + "` while expecting `^` or `_`.");
-			return null;
-		}
+        continue;
+      }
 
-		if(state === 1){
-			var ch = conv_obj[info[cnt]];
-			if(ch == null){
-				error("Parse error: unexpected `" + info[cnt] 
-					+ "` while expecting `!`, `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `#` or `$`.");
-				return null;
-			}
+      error(
+        "Parse error: unexpected `" +
+          info[cnt] +
+          "` while expecting `^` or `_`."
+      );
+      return null;
+    }
 
-			tmp.txt = ch;
-			state++;
-			continue;
-		}
+    if (state === 1) {
+      var ch = conv_obj[info[cnt]];
+      if (ch == null) {
+        error(
+          "Parse error: unexpected `" +
+            info[cnt] +
+            "` while expecting `!`, `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `#` or `$`."
+        );
+        return null;
+      }
 
-		if(state === 2){
-			if(info[cnt] === "k" || info[cnt] === "h") {
-				tmp.isRed = (info[cnt] === "k");
-				res_arr[res_arr.length] = JSON.parse(JSON.stringify(tmp));
-				tmp = {};
-				state = 0;
-				continue;
-			}
-		}
+      tmp.txt = ch;
+      state++;
+      continue;
+    }
 
-		throw new Error("cannot happen");
-	}
-	return res_arr;
+    if (state === 2) {
+      if (info[cnt] === "k" || info[cnt] === "h") {
+        tmp.isRed = info[cnt] === "k";
+        res_arr[res_arr.length] = JSON.parse(JSON.stringify(tmp));
+        tmp = {};
+        state = 0;
+        continue;
+      }
+    }
+
+    throw new Error("cannot happen");
+  }
+  return res_arr;
 }
